a04d31f6e5f2679ccb840e461f36fa08d9cda0b0
Станислав Зверьков <zverk3333@gmail.com>
2022-04-04 17:21:25 +0300

    user defaults Added, app theme changing added, signIn/Out handling added

--- SettingsSet/SettingsTableViewController.swift
+++ SettingsSet/SettingsTableViewController.swift
@@ -24,6 +24,56 @@ class SettingsTableViewController: UITableViewController {
         super.viewDidLoad()
         
     }
+    
+    //    MARK:  App theme
+    
+    @IBAction func segmentedControlAction(_ sender: Any) {
+        switch appThemeSegmentedControl.selectedSegmentIndex{
+        case 0: tabBarController?.overrideUserInterfaceStyle = .unspecified
+            appSettings.appTheme = 0
+        case 1: tabBarController?.overrideUserInterfaceStyle = .dark
+            appSettings.appTheme = 1
+        case 2: tabBarController?.overrideUserInterfaceStyle = .light
+            appSettings.appTheme = 2
+        default:
+            tabBarController?.overrideUserInterfaceStyle = .unspecified
+        }
+    }
+    
+    //    MARK:  SingIn & SignOut
+    
+    
+    @IBAction func signInButtonAction(_ sender: Any) {
+        
+        if appSettings.signedIn {
+            let alert = UIAlertController(title: NSLocalizedString("Are You shure?", comment: ""), message: NSLocalizedString("Do You want to logOut?", comment: ""), preferredStyle: .alert)
+            
+            let alertYesAction = UIAlertAction(title: NSLocalizedString("Yes", comment: ""), style: .destructive) { UIAlertAction in
+                self.fireAPI.signOut()
+                self.signInButton.setTitle(NSLocalizedString("Sign in", comment: ""), for: .normal)
+                self.accountLabel.text = NSLocalizedString("authorization", comment: "")
+            }
+            
+            let alertCancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .default, handler: nil)
+            
+            alert.addAction(alertCancelAction)
+            alert.addAction(alertYesAction)
+            
+            present(alert, animated: true, completion: nil)
+
+        } else {
+            let AuthorisationVС = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "AuthorisationViewController") as? AuthorisationViewController
+            
+            self.navigationController?.pushViewController(AuthorisationVС!, animated: true)
+        }
+    }
+    
+//    MARK: - Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let webView = segue.destination as! WEBViewController
+        webView.openWEBForURl(url: WEBurl)
+    }
 
     
 // MARK: - Table view data source
--- StoreApp/Model/Local/AppSettings.swift
+++ StoreApp/Model/Local/AppSettings.swift
@@ -8,5 +8,69 @@
 import Foundation
 
 class AppSettings{
-    var userID = ""
+    
+    let userDefaults = UserDefaults.standard
+    
+    private enum SettingsKeys: String{
+        case userEmail = "userEmail"
+        case appTheme = "AppTheme"
+        case isSignIn = "signedIn"
+//        case language = "Language"
+//        case userID = "userID"
+    }
+    
+    var userEmail: String{
+        get{
+            if let data = userDefaults.string(forKey: SettingsKeys.userEmail.rawValue){
+                return data
+            }else{
+                return ""
+            }
+        }
+        set{
+            userDefaults.set(newValue, forKey: SettingsKeys.userEmail.rawValue)
+        }
+    }
+    
+    var signedIn: Bool{
+        get{
+            return userDefaults.bool(forKey: SettingsKeys.isSignIn.rawValue)
+        }
+        set{
+            userDefaults.set(newValue, forKey: SettingsKeys.isSignIn.rawValue)
+        }
+    }
+    
+    var appTheme: Int {
+        get{
+            return userDefaults.integer(forKey: SettingsKeys.appTheme.rawValue)
+        }
+        set{
+            userDefaults.set(newValue, forKey: SettingsKeys.appTheme.rawValue)
+        }
+    }
+//    var language: String{
+//        get{
+//            if let data = userDefaults.string(forKey: SettingsKeys.language.rawValue){
+//                return data
+//            } else {
+//                return ""
+//            }
+//        }
+//        set{
+//            if newValue != ""{
+//                userDefaults.set(newValue, forKey: SettingsKeys.language.rawValue)
+//            }
+//        }
+//    }
+    
+//    var userID: String{
+//        get{
+//            return userDefaults.string(forKey: SettingsKeys.userID.rawValue) ?? ""
+//        }
+//        set{
+//            userDefaults.set(newValue, forKey: SettingsKeys.userID.rawValue)
+//        }
+//    }
+    
 }
