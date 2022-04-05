185ff45f41f023927e3d4d63e7fbeaab99b3d9ed
Станислав Зверьков <zverk3333@gmail.com>
2022-04-05 19:27:52 +0300

    registration VC configured

--- RegistrationViewController.swift
+++ RegistrationViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var fullNameTextFiedl: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    
+    let validator = Validator()
+    let fireAPI = APIManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.validationLabel.isHidden = true
     }
     
+    @IBAction func registrationButton(_ sender: Any) {
+        
+        if validator.userDataIsCurrect(email: emailTextField.text, pass: emailTextField.text) {
 
-    /*
-    // MARK: - Navigation
+            fireAPI.registration(email: emailTextField.text!, password: passwordTextField.text!) { regSuccess in
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                if !regSuccess {
+                    let alert = UIAlertController(title: NSLocalizedString("Registration Error", comment: ""), message: NSLocalizedString("Network unable or email already exist.", comment: ""), preferredStyle: .alert)
+
+                    let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+
+                    alert.addAction(alertAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        } else {
+            self.validationLabel.isHidden = false
+            self.validationLabel.text = NSLocalizedString("Check your email and password.", comment: "")
+        }
     }
-    */
-
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+ 
 }
