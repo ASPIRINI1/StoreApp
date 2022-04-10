9fc62fb09ec7bd5474fec3b965262cce6c27ce04
Станислав Зверьков <zverk3333@gmail.com>
2022-04-10 23:13:17 +0300

    userInfoChangingVC keyboard dissmissing & textField name fix

--- SettingsSet/UserInfoChangeViewController.swift
+++ SettingsSet/UserInfoChangeViewController.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class UserInfoChangeViewController: UIViewController {
 
+//    MARK: Outlets
+    
     @IBOutlet weak var headerLAbel: UILabel!
     @IBOutlet weak var firstLabel: UILabel!
-    @IBOutlet weak var firstTextBox: UITextField!
+    @IBOutlet weak var firstTextField: UITextField!
     @IBOutlet weak var secondLabel: UILabel!
-    @IBOutlet weak var secondTextBox: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
     @IBOutlet weak var thirdLabel: UILabel!
-    @IBOutlet weak var thirdTextBox: UITextField!
+    @IBOutlet weak var thirdTextField: UITextField!
     
     enum changingType {
         case email
         case password
         case address
     }
+    
+//    MARK: - Properties
     
     private var viewType = changingType.email
     private let validator = Validator()
@@ -31,45 +35,50 @@ class UserInfoChangeViewController: UIViewController {
         super.viewDidLoad()
         createView()
         
+        let tapScreen = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapScreen.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapScreen)
     }
+    
+//    MARK: - Actions
     
     @IBAction func buttonAction(_ sender: Any) {
         switch viewType {
         case .email:
             print("email")
-            if ((firstTextBox.text?.isValidEmail()) != nil) {
+            if ((firstTextField.text?.isValidEmail()) != nil) {
                 
             } else {
                 let alert = UIAlertController(title: NSLocalizedString("Wrong Email.", comment: ""), message: nil, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    self.firstTextBox.text?.removeAll()
+                    self.firstTextField.text?.removeAll()
                 }))
             }
             
         case .password:
             print("password")
-            if ((secondTextBox.text?.isValidPass()) != nil) {
+            if ((secondTextField.text?.isValidPass()) != nil) {
                 
             } else {
                 let alert = UIAlertController(title: NSLocalizedString("Wrong password.", comment: ""), message: nil, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    self.secondTextBox.text?.removeAll()
+                    self.secondTextField.text?.removeAll()
                 }))
             }
             
-            if thirdTextBox.text == secondTextBox.text {
+            if thirdTextField.text == secondTextField.text {
                 
             } else {
                 let alert = UIAlertController(title: NSLocalizedString("Passwords do not same.", comment: ""), message: nil, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    self.thirdTextBox.text?.removeAll()
+                    self.thirdTextField.text?.removeAll()
                 }))
             }
             
         case .address:
             print("address")
-            if firstTextBox.text != nil {
-                appSettings.userAdress = firstTextBox.text ?? ""
+            if firstTextField.text != nil {
+                appSettings.userAdress = firstTextField.text ?? ""
             } else {
                 let alert = UIAlertController(title: NSLocalizedString("Adress changing fail.", comment: ""), message: nil, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default))
@@ -78,6 +87,8 @@ class UserInfoChangeViewController: UIViewController {
         }
     }
     
+//    MARK: - Additional funcs
+    
     private func createView() {
         
         switch viewType {
@@ -85,30 +96,39 @@ class UserInfoChangeViewController: UIViewController {
         case .email:
             headerLAbel.text = NSLocalizedString("You must confirm the change of email on your new mailbox.", comment: "")
             firstLabel.text = NSLocalizedString("Your new email", comment: "")
-            firstTextBox.placeholder = NSLocalizedString("Enter email here", comment: "")
+            firstTextField.placeholder = NSLocalizedString("Enter email here", comment: "")
             secondLabel.text = NSLocalizedString("Enter your passord", comment: "")
-            secondTextBox.placeholder = NSLocalizedString("password", comment: "")
+            secondTextField.placeholder = NSLocalizedString("password", comment: "")
             
         case .password:
             headerLAbel.text = NSLocalizedString("You must confirm the change of password on your mailbox.", comment: "")
             firstLabel.text = NSLocalizedString("Old password", comment: "")
-            firstTextBox.placeholder = NSLocalizedString("Enter old passord here", comment: "")
+            firstTextField.placeholder = NSLocalizedString("Enter old passord here", comment: "")
             secondLabel.text = NSLocalizedString("New password", comment: "")
-            secondTextBox.placeholder = NSLocalizedString("Enter new passord here", comment: "")
+            secondTextField.placeholder = NSLocalizedString("Enter new passord here", comment: "")
             thirdLabel.isHidden = false
-            thirdTextBox.isHidden = false
+            thirdTextField.isHidden = false
             thirdLabel.text = NSLocalizedString("Write new password again", comment: "")
             
         case .address:
             headerLAbel.text = NSLocalizedString("You must confirm the change of address on your mailbox.", comment: "")
             firstLabel.text = NSLocalizedString("Adress", comment: "")
-            firstTextBox.placeholder = NSLocalizedString("Enter old passord here", comment: "")
+            firstTextField.placeholder = NSLocalizedString("Enter old passord here", comment: "")
             secondLabel.isHidden = true
-            secondTextBox.isHidden = true
+            secondTextField.isHidden = true
         }
     }
     
     func setViewType(viewType: changingType) {
         self.viewType = viewType
     }
+    
+    @objc func dismissKeyboard(selector: UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    
+}
+
+extension UserInfoChangeViewController: UITextFieldDelegate {
+    
 }
