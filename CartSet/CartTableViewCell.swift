fbd626d7f2ec743db105161ffbe0bb4c028a1f12
Станислав Зверьков <zverk3333@gmail.com>
2022-04-14 22:25:25 +0300

    cartTableVC configured

--- CartSet/CartTableViewCell.swift
+++ CartSet/CartTableViewCell.swift
@@ -13,16 +13,5 @@ class CartTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var priceAlbel: UILabel!
     @IBOutlet weak var countLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
